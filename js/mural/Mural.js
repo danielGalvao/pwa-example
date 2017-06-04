f12408d23910391f0fcd5b1fcfd6cc702c11f53d
Daniel Galvão <galvaodev@gmail.com>
2017-06-04 00:04:46 -0300

    Add login logic to add card

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -6,15 +6,17 @@ const Mural = (function(_render, Filtro){
     Filtro.on("filtrado", render)
 
     function adiciona(cartao){
+      if(logado) {
         cartoes.push(cartao)
         cartao.on("mudanca.**", render)
-        cartao.on("remocao", ()=>{
-            cartoes = cartoes.slice(0)
-            cartoes.splice(cartoes.indexOf(cartao),1)
-            render()
+        cartao.on("remocao", () => {
+          cartoes = cartoes.slice(0)
+          cartoes.splice(cartoes.indexOf(cartao),1)
+          render()
         })
         render()
         return true
+      }
     }
 
     return Object.seal({
