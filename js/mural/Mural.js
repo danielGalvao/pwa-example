b189ba59d13044d37cd4c92dbeee52a9ae7b57bd
Daniel Galvão <daniel.galvao@justdigital.com.br>
2017-06-06 08:37:22 -0300

    Fix cards and add localstorage to remove or update card

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,30 +1,43 @@
 const Mural = (function(_render, Filtro){
     "use strict"
-    let cartoes = JSON.parse(localStorage.getItem("cartoes")).map(localCartao => new Cartao(localCartao.conteudo, localCartao.tipo)) || []
+    let cartoes = JSON.parse(localStorage.getItem("cartoes"));
+    cartoes = cartoes ? cartoes.map(localCartao => new Cartao(localCartao.conteudo, localCartao.tipo)) : []
+    cartoes.forEach(cartao => {
+      preparaCartao(cartao)
+    })
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
     render()
 
     Filtro.on("filtrado", render)
 
+    function preparaCartao(cartao) {
+      cartao.on("mudanca.**", salvaCartoes)
+      cartao.on("remocao", () => {
+        cartoes = cartoes.slice(0)
+        cartoes.splice(cartoes.indexOf(cartao),1)
+        salvaCartoes()
+        render()
+      })
+    }
+
+    function salvaCartoes() {
+      localStorage.setItem("cartoes",
+        JSON.stringify(
+          cartoes.map(
+            cartao => ({
+              conteudo: cartao.conteudo,
+              tipo: cartao.tipo
+            })
+          )
+        )
+      )
+    }
+
     function adiciona(cartao){
       if(logado) {
         cartoes.push(cartao)
-        localStorage.setItem("cartoes",
-          JSON.stringify(
-            cartoes.map(
-              cartao => ({
-                conteudo: cartao.conteudo,
-                tipo: cartao.tipo
-              })
-            )
-          )
-        )
-        cartao.on("mudanca.**", render)
-        cartao.on("remocao", () => {
-          cartoes = cartoes.slice(0)
-          cartoes.splice(cartoes.indexOf(cartao),1)
-          render()
-        })
+        salvaCartoes()
+        preparaCartao(cartao)
         render()
         return true
       }
